293cdf88c6e7bf9e2f0d115f560a9edbca685d8c
Jaliborc <contact@jaliborc.com>
2013-06-22 11:54:12 +0100

    Fixed issue with unit tests

--- Tests.lua
+++ Tests.lua
@@ -1,17 +1,22 @@
-local WoWTest = LibStub('WoWTest-1.0')
 local Replace = WoWTest.Replace
-local Tests = WoWTest:New()
+local Tests = WoWTest('Scrap')
 
 local function NotJunk(id)
+	Replace('Scrap_Junk', Scrap_BaseList)
 	Replace('Scrap_LowConsume', true)
 	Replace('Scrap_LowEquip', true)
 
 	return WoWTest.IsFalse(
-		Scrap:CheckFilters(id))
+		Scrap:IsJunk(id))
 end
 
 function Tests:ChefHat()
 	NotJunk(46349)
+end
+
+function Tests:Fishing()
+	NotJunk(33820)
+	NotJunk(19969)
 end
 
 function Tests:VanityUsable()
