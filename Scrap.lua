5804e3f49597f1e289065212ae3cf2b6022d8851
Brian Salvaggio <brian.salvaggio@dycominc.com>
2018-07-18 20:24:35 -0400

    @belegcufea fix for 8.0.1

--- Scrap.lua
+++ Scrap.lua
@@ -269,7 +269,7 @@ function Scrap:LoadTooltip(link, bag, slot)
 end
 
 function Scrap:BelongsToSet()
-	return CanUseEquipmentSets() and GetLine(self.numLines - 1):find(IN_SET)
+	return C_EquipmentSet.CanUseEquipmentSets() and GetLine(self.numLines - 1):find(IN_SET)
 end
 
 function Scrap:IsSoulbound(bag, slot)
