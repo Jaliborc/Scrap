f492de4bca371a660a99910515a4d1648a3ce8c9
Joao Cardoso <442637+Jaliborc@users.noreply.github.com>
2022-11-29 21:31:20 +0100

    Updated spanish locale

--- localization/es.lua
+++ localization/es.lua
@@ -1,13 +1,56 @@
 local L = LibStub('AceLocale-3.0'):NewLocale('Scrap', 'esES') or LibStub('AceLocale-3.0'):NewLocale('Scrap', 'esMX')
 if not L then return end
 
-L.Add = 'Añadir a la Lista de Basura'
-L.Added = 'Añadido a la Lista de Basura: %s'
-L.AutoSell = 'Auto Vender'
-L.AutoSellTip = 'Si está habilitado, Scrap venderá automáticamente toda tu basura cuando visites a un mercader.'
+-- main
+L.Add = 'Añadir a la lista de basura'
+L.Added = 'Añadido a la lista de basura: %s'
+L.DestroyJunk = 'Destruir basura'
+L.SellJunk = 'Vender basura'
+L.SoldJunk = 'Has vendido basura por un valor de: %s'
+L.Remove = 'Retirar de la lista de basura'
+L.Removed = 'Has retirado de la lista de basura: %s'
+L.Repaired = 'Has reparado tu armadura por un valor de: %s'
+L.ToggleMousehover = 'Alternar artículo bajo el cursor'
+
+-- dialogs
+L.ConfirmDelete = '¿Estás seguro de que quieres destruir todos tus items basura? ¡No puedes deshacer esta acción!.'
+
+-- visualizer
 L.Junk = 'Basura'
-L.Remove = 'Eliminar de la Lista de Basura'
-L.Removed = 'Eliminado de la Lista de Basura: %s'
-L.SellJunk = 'Vender Basura'
-L.SoldJunk = 'Has vendido tu basura por %s'
-L.Repaired = 'Has reparado tu equipamento por %s'
+L.NotJunk = 'Útil'
+
+-- options
+L.AdvancedOptions = 'Opciones avanzadas'
+L.CharSpecific = 'Lista de basura específica del personaje'
+L.Description = 'Estas opciones te permiten configurar Scrap aún más. ¡La basura no dominará tus bolsas!'
+
+
+L.AutoSell = 'Vender automáticamente'
+L.AutoSellTip = 'Si está activado, Scrap venderá automáticamente tu basura cuando visites a un mercader.'
+L.AutoRepair = 'Reparar automáticamente'
+L.AutoRepairTip = 'Si está activado, Scrap reparará automáticamente tu armadura cuando visites a un mercader.'
+L.DestroyWorthless = 'Destruir objetos sin valor'
+L.DestroyWorthlessTip = 'Si está activado, Scrap destruirá los items basura sin valor de venta.'
+L.GuildRepair = 'Usar fondos de la hermandad'
+L.GuildRepairTip = 'Si está activado, Scrap utilizará los fondos de la hermandad disponibles para reparaciones antes que tu propio oro.'
+L.Glow = 'Bordes brillantes'
+L.GlowTip = 'Si está activado, aparecerán bordes brillantes en tus items basura.'
+L.Icons = 'Icono de moneda'
+L.IconsTip = 'Si está activado, aparecerá un pequeño icono de moneda de oro en tus items basura.'
+L.Learning = 'Aprendizaje de uso'
+L.LearningTip = 'Si está activado, Scrap sabrá qué items le vendes habitualmente al mercader y automáticamente los considerará basura.'
+L.LowConsume = 'Consumibles de bajo nivel'
+L.LowConsumeTip = 'Si está activado, Scrap venderá los consumibles que son demasiado bajos para tu nivel.'
+L.LowEquip = 'Equipo de bajo nivel'
+L.LowEquipTip = 'Si está activado, Scrap venderá el equipo ligado al alma que tiene un nivel mucho menor que el que estás usando .'
+L.SafeMode = 'Modo seguro'
+L.SafeModeTip = 'Si está activado, Scrap no venderá más de 12 items a la vez, por lo que siempre podrás recuperarlos.'
+L.Unusable = 'Equipo inutilizable'
+L.UnusableTip = 'Si está activado, Scrap venderá el equipo ligado al alma, que nunca podría ser usado por tu personaje.'
+
+-- tutorials
+L.Tutorial_Welcome = 'Bienvenido a |cffffd200Scrap|r, la solución inteligente de venta de basura de |cffffd200Jaliborc|r. Este breve tutorial te ayudará a comenzar a vender tus items basura.|n|nLo que te ahorrará tiempo y tus bolsillos sin duda lo agradecerán. ¿Empezamos?'
+L.Tutorial_Button = 'Scrap venderá automáticamente toda tu basura cada vez que visites a un mercader. Pero puedes venderla manualmente, es muy simple: |cffffd200Click|r en el botón Scrap.|n|n|cffffd200Click derecho|r en el botón para ver opciones adicionales.'
+L.Tutorial_Drag = '¿Qué pasa si quieres decirle a Scrap cuales items vender o no? es muy simple: |cffffd200Arrastra|r los items de tus bolsas al botón Scrap.|n|nAlternativamente, puedes establecer un |cffffd200Keybinding|r en las opciones de |cffffd200Menú de juego|r. Presiónalo mientras colocas el item.'
+L.Tutorial_Visualizer = 'Para ver qué items has especificado como basura o no, abre el visualizador en la pestaña |cffffd200Scrap|r.|n|nTen en cuenta que solo mostrará los items que tienes |cffffd200especificados|r, no todos los items del juego.'
+L.Tutorial_Bye = 'Buena suerte en tus viajes y que |cffb400fflos épicos|r estén contigo. ¡La basura no dominará tus bolsas!'
